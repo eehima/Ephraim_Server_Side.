9f862a67e9573f954b9514a1fd862b895930e728
Ephraim Ehima <ehima87@gmail.com>
2026-04-24 21:36:36 +0100

    addedCommit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// import express from express
 const express = require("express");
+// create a port
 const port = 7070;
-
+//create instance of express
 const app = express();
 
+// 
 app.use(express.json());
+
+// An array of users
 const users = [
     {"id": 1, "name": "Alice", "age": 30},
     {"id": 2, "name": "Bob", "age": 25},
     {"id": 3, "name": "Charlie", "age": 35}
 ]
 
+// a route of getting all users
 app.get('/users', (req, res) => {
     res.status(200).json(users);
 });
@@ -30,6 +36,7 @@ app.get('/', (req, res) => {
     res.send('Server is up and running.');
 });
 
+// a route of creating a new user using POST.
 app.post('/new-users', (req, res) => {
     const {name, email} = req.body;
 
@@ -41,11 +48,12 @@ app.post('/new-users', (req, res) => {
         name: name,
         email: email
     };
-    
+
     users.push(newUser);
     res.status(201).json(newUser);
     });
 
+    // Port listening
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`); 
 
